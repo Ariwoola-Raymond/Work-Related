36787203cab69a21730410e8868675dab4c57ee4
Raymond Ariwoola <Raymondariwoola@gmail.com>
2023-02-11 18:24:14 +0400

    Update file

--- Development/Python/experimental/update_file.py
+++ Development/Python/experimental/update_file.py
@@ -1,13 +1,21 @@
 import time
 import os
+import random
 
 filename = "/Users/raymond/Documents/GitHub/Work Related/file.txt"
 repo_path = "/Users/raymond/Documents/GitHub/Work Related"
+quotes = [
+    "The best way to predict the future is to create it. - Abraham Lincoln",
+    "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+    "You can't build a reputation on what you are going to do. - Henry Ford",
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill",
+]
 
 while True:
     with open(filename, "w") as f:
         # Update the contents of the file
-        f.write("Current time: " + str(time.time()))
+        f.write("Current time: " + str(time.time()) + "\n Current Quote: " + random.choice(quotes))
 
     os.chdir(repo_path)
     # os.system("git add " + filename)
