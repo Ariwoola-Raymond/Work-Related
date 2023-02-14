1d3bffd08a423d1301d36aae7874cd5619ea6812
Raymond Ariwoola <Raymondariwoola@gmail.com>
2023-02-14 11:38:02 +0400

    update vb script

--- MS Excel/VBA/CopyCells.vb
+++ MS Excel/VBA/CopyCells.vb
@@ -1,3 +1,4 @@
+' This modified script uses a For loop to repeat the copy and transpose operation for each row in the range A2:E(lastRow) on Sheet1. The variable destRow is used to specify the row number in the destination range on Sheet2, which starts at row 2 and is incremented by 2 at the end of each iteration of the loop to leave an empty row between each paste operation. The Copy and PasteSpecial methods are called within the For loop for each row, using the i variable to specify the row number in the range being copied.
 Sub CopyCells()
     Dim lastRow As Long
     lastRow = Sheets("Sheet1").Cells(Rows.Count, "A").End(xlUp).Row
@@ -11,4 +12,22 @@ Sub CopyCells()
     Next i
 End Sub
 
-' This modified script uses a For loop to repeat the copy and transpose operation for each row in the range A2:E(lastRow) on Sheet1. The variable destRow is used to specify the row number in the destination range on Sheet2, which starts at row 2 and is incremented by 2 at the end of each iteration of the loop to leave an empty row between each paste operation. The Copy and PasteSpecial methods are called within the For loop for each row, using the i variable to specify the row number in the range being copied.
+
+' updated version of the script
+Sub CopyCells()
+    Dim lastRow As Long
+    lastRow = Sheets("Sheet1").Cells(Rows.Count, "A").End(xlUp).Row
+    Dim destRow As Long
+    destRow = 2
+    
+    For i = 2 To lastRow
+        Dim pasteRange As Range
+        Set pasteRange = Sheets("Sheet2").Range("A" & destRow)
+        pasteRange.Value = Sheets("Sheet1").Range("A" & i).Value
+        pasteRange.Offset(0, 1).Value = Sheets("Sheet1").Range("B" & i).Value
+        pasteRange.Offset(0, 2).Value = Sheets("Sheet1").Range("C" & i).Value
+        pasteRange.Offset(0, 3).Value = Sheets("Sheet1").Range("D" & i).Value
+        pasteRange.Offset(0, 4).Value = Sheets("Sheet1").Range("E" & i).Value
+        destRow = destRow + 2 ' Add 2 to destination row to leave an empty row
+    Next i
+End Sub
