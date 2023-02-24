e82a3c3d6253d84b2df6ac3197710379022d9c6d
Raymond Ariwoola <Raymondariwoola@gmail.com>
2023-02-24 12:33:10 +0400

    update

--- Development/Batch/LAN_settings.bat
+++ Development/Batch/LAN_settings.bat
@@ -4,3 +4,6 @@ cmd.exe /c "start control.exe /name Microsoft.InternetOptions /page pageConnecti
 
 
 cmd.exe /c "start rundll32.exe shell32.dll,Control_RunDLL inetcpl.cpl,,4 && timeout /T 1 /NOBREAK && powershell.exe -Command \"Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{TAB}{TAB}{SPACE}{TAB}{TAB}{TAB}{ENTER}');\""
+
+
+cmd.exe /c "start inetcpl.cpl,,4 && timeout /T 1 /NOBREAK && powershell.exe -Command \"Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{TAB}{TAB}{SPACE}{TAB}{TAB}{TAB}{ENTER}');\""
