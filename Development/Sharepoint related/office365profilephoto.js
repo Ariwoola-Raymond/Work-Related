573e8fd67c3e461447249ffbccf9afd368076bdd
Raymond Ariwoola <Raymondariwoola@gmail.com>
2023-03-15 13:58:30 +0400

    sharepoint

--- Development/Sharepoint related/office365profilephoto.js
+++ Development/Sharepoint related/office365profilephoto.js
@@ -43,3 +43,54 @@ const onAccessTokenError = (error) => {
 
 // Get the access token using the SharePoint context
 const accessToken = await SP.RequestExecutorUtilities.getAccessToken(context, appContextSite.get_url(), onAccessTokenSuccess, onAccessTokenError);
+
+
+
+
+//////////////////////////////////////////////////////////////////// //////////////////// //////////////////// /////////////////// /////////////////// /////////////////// /////////////////// /////////////////// /////////////////// /////////////////// /////////////////// ///////////////////
+
+// Define the user ID or email address of the user whose photo you want to retrieve
+const userId = "john.doe@contoso.com";
+
+// Construct the URL to the user's photo
+const photoUrl = `https://graph.microsoft.com/v1.0/users/${userId}/photo/$value`;
+
+// Get the access token using the SharePoint context
+const getAccessToken = (callback) => {
+    // Get the SharePoint context
+    const context = SP.ClientContext.get_current();
+    const web = context.get_web();
+    const appContextSite = new SP.AppContextSite(context, 'https://your-sharepoint-site-url');
+    
+    // Get the access token using the SharePoint context
+    SP.RequestExecutorUtilities.getAccessToken(context, appContextSite.get_web().get_url(), (accessToken) => {
+        callback(accessToken);
+    });
+};
+
+// Retrieve the user's photo using the access token
+const getPhoto = (accessToken) => {
+    // Set the authorization header with the access token
+    const headers = {
+        Authorization: `Bearer ${accessToken}`
+    };
+    
+    // Retrieve the user's photo using jQuery
+    $.ajax({
+        url: photoUrl,
+        headers: headers,
+        success: (data) => {
+            // Display the photo on the page
+            const photoElement = $("<img>").attr("src", URL.createObjectURL(data));
+            $("body").append(photoElement);
+        },
+        error: (error) => {
+            console.error(error);
+        }
+    });
+};
+
+// Call the functions to retrieve the access token and user's photo
+getAccessToken((accessToken) => {
+    getPhoto(accessToken);
+});
